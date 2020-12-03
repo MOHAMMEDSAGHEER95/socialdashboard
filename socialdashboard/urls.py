e88b23771f43f412c982d69fcb19bdf23d3a201e
mohammedsagheer95 <sagheer.sayone@gmail.com>
2020-12-03 07:48:29 +0530

    api added to url

--- socialdashboard/urls.py
+++ socialdashboard/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashboard/', include('applications.dashboard.urls')),
+    path('api/', include('applications.dashboard.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
