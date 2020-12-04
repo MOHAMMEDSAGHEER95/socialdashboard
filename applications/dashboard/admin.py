46dc6bb30cd4b492c16dab8e6c4e4e7a4f640828
mohammedsagheer95 <sagheer.sayone@gmail.com>
2020-12-05 00:14:38 +0530

    added admin

--- applications/dashboard/admin.py
+++ applications/dashboard/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from applications.dashboard.models import SocialMediaAccessToken
 
+
+class SocialMediaAccessTokenAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created', 'modified')
+
+
 admin.site.register(SocialMediaAccessToken)
